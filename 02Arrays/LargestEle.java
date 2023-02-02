6ff84ae7c9bd059c4477ff68ed1e3f93349ed6ce
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2023-02-02 13:04:29 +0530

    Create LargestEle.java

---
+++ 02Arrays/LargestEle.java
@@ -0,0 +1,13 @@
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {   int max= arr[0];
+        for(int i=0; i<arr.length;i++){
+           
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
