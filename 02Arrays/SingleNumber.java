afe398b932a6315c4a84f154cb11b2cfdad52b46
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2023-02-06 18:07:41 +0530

    Create SingleNumber.java

---
+++ 02Arrays/SingleNumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
+   
