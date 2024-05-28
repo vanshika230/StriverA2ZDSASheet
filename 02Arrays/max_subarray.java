1755beec675de71eba4393ac759447763f643533
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2024-05-28 12:33:11 +0530

    Create max_subarray.java

---
+++ 02Arrays/max_subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int maxsum=nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum > maxsum){
+                maxsum=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxsum;
+    }
+}
